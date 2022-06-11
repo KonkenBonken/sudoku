cc86c7891733d514becbd06d909b9c62b4016ad9
KonkenBonken <konrad05pettersson@gmail.com>
2022-06-11 16:28:05 +0200

    Fixed value range

--- src/Sudoku.tsx
+++ src/Sudoku.tsx
@@ -9,7 +9,7 @@ class Sudoku {
     for (let y = 0; y < 9; y++) {
       const row = <div class="row" />;
       this.el.append(row);
-      this.cells[y] = Array(9).fill(0).map((_, x) => new Cell({ x, y }, Math.floor(Math.random() * 10)));
+      this.cells[y] = Array(9).fill(0).map((_, x) => new Cell({ x, y }, Math.floor(Math.random() * 9 + 1)));
       row.append(...this.cells[y].map(({ el }) => el));
     }
 
